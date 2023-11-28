5af4cd30384d86c0ff36fc9d33688796786d64bc
mahad <121475272@umail.ucc.ie>
2023-11-28 23:20:56 +0000

    Created a constructor for a ColourTable object with a single parameter that specifies the number of colors in the palette

--- src/main/java/ColourTable.java
+++ src/main/java/ColourTable.java
@@ -1,2 +1,63 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class ColourTable {
+
+    private final int paletteSize;
+    public ColourTable(int paletteSize) {
+        if (!isValidPaletteSize(paletteSize)) {
+            throw new IllegalArgumentException("Invalid palette size. Must be a power of two, greater than 1, and less than 1025");
+        }
+        this.paletteSize = paletteSize;
+    }
+    private boolean isValidPaletteSize(int size) {
+        return size > 1 && (size & (size - 1)) == 0 && size < 1025;
+    }
 }
+//    private final int paletteSize;
+//    private final Set<RGBColor> colors = new HashSet<>();
+//
+//    public ColourTable(int paletteSize) {
+//        if (!isValidPaletteSize(paletteSize)) {
+//            throw new IllegalArgumentException("Invalid palette size. Must be a power of two, greater than 1, and less than 1025");
+//        }
+//        this.paletteSize = paletteSize;
+//    }
+//
+//    public void add(int red, int green, int blue) {
+//        RGBColor rgbColor = new RGBColor(red, green, blue);
+//        if (!isValidRGB(rgbColor)) {
+//            throw new IllegalArgumentException("Invalid RGB color");
+//        }
+//
+//        if (colors.size() < paletteSize) {
+//            if (!colors.contains(rgbColor)) {
+//                colors.add(rgbColor);
+//            } else {
+//                throw new IllegalStateException("Color already exists in the ColourTable");
+//            }
+//        } else {
+//            throw new IllegalStateException("Exceeded the capacity of the ColourTable");
+//        }
+//    }
+//    private boolean isValidRGB(RGBColor rgbColor) {
+//        return rgbColor.getRed() >= 0 && rgbColor.getRed() <= 255 &&
+//                rgbColor.getGreen() >= 0 && rgbColor.getGreen() <= 255 &&
+//                rgbColor.getBlue() >= 0 && rgbColor.getBlue() <= 255;
+//    }
+//}
+//import java.util.HashSet;
+//import java.util.Set;
+
+//public class ColourTable {
+//    private Set<RGBColor> colors = new HashSet<>();
+//
+//    public void add(RGBColor rgbColor) {
+//        colors.add(rgbColor);
+//    }
+//    public int getNumberOfColors() {
+//        return colors.size();
+//    }
+//}
+
