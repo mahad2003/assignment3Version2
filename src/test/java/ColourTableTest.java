15a132fc374ee1bd8e58cc24867bc765ff4b08cf
mahad <121475272@umail.ucc.ie>
2023-11-28 22:56:28 +0000

    Wrote a test to add color and added a junit 5 configuration to the project

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,2 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ColourTableTest {
+    public void testAddColor() {
+        ColourTable colourTable = new ColourTable(4);
+        colourTable.add(255, 0, 0); // Red
+        assertEquals(1, colourTable.getNumberOfColors());
+    }
 }
