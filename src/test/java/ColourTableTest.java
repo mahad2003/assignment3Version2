c61dbc776c4fc44ac29f79c1bfc8fa1f87b3ec6e
mahad <121475272@umail.ucc.ie>
2023-11-28 23:38:23 +0000

    Created testAddMultipleColors which tests the add method of ColourTable for adding multiple colors

--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -2,9 +2,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ColourTableTest {
+
+    @Test
     public void testAddColor() {
         ColourTable colourTable = new ColourTable(4);
         colourTable.add(255, 0, 0); // Red
         assertEquals(1, colourTable.getNumberOfColors());
+    }
+
+    @Test
+    public void testAddMultipleColors() {
+        ColourTable colourTable = new ColourTable(4);
+        colourTable.add(255, 0, 0); // Red
+        colourTable.add(0, 255, 0); // Green
+        colourTable.add(0, 0, 255); // Blue
+        assertEquals(3, colourTable.getNumberOfColors());
     }
 }
