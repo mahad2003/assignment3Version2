5d3750701608fee6d429d2dd85e2308c132354d7
mahad <121475272@umail.ucc.ie>
2023-11-28 23:25:53 +0000

    Created the constructor in RGBColor and declared variables for red, green and blue

--- src/main/java/RGBColor.java
+++ src/main/java/RGBColor.java
@@ -1,2 +1,11 @@
 public class RGBColor {
+    private final int red;
+    private final int green;
+    private final int blue;
+
+    public RGBColor(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
 }
